bc968e70d9a4f42610b303e67fe0a717800ab332
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-05-31 19:39:46 +0300

    python keras utils update

--- keras_vggface/models.py
+++ keras_vggface/models.py
@@ -13,8 +13,8 @@ from tensorflow.keras.layers import Flatten, Dense, Input, GlobalAveragePooling2
     GlobalMaxPooling2D, Activation, Conv2D, MaxPooling2D, BatchNormalization, \
     AveragePooling2D, Reshape, Permute, multiply
 from keras_applications.imagenet_utils import _obtain_input_shape
-from tensorflow.keras.utils import layer_utils
-from tensorflow.keras.utils.data_utils import get_file
+from tensorflow.python.keras.utils import layer_utils
+from tensorflow.python.keras.utils.data_utils import get_file
 from tensorflow.keras import backend as K
 from tensorflow.keras_vggface import utils
 from tensorflow.keras.engine.topology import get_source_inputs
